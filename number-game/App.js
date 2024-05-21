0f0b596ccf0ddc53d3c245e1fedfbecb22565654
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-05-21 11:21:54 -0300

    Adding fonts and loading screen

--- number-game/App.js
+++ number-game/App.js
@@ -1,15 +1,21 @@
-import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native'
+import { StyleSheet, ImageBackground, SafeAreaView, Text } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
+import { useState } from 'react'
+import { useFonts } from 'expo-font'
 
 import StartGameScreen from './screens/StartGameScreen'
 import GameScreen from './screens/GameScreen'
 import GameOverScreen from './screens/GameOverScreen'
-import { useState } from 'react'
 import Colors from './constants/colors'
 
 export default function App() {
   const [userNumber, setUserNumber] = useState(null)
   const [gameIsOver, setGameIsOver] = useState(false)
+
+  const [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  })
 
   function pickedNumberHandler(pickedNumber) {
     setUserNumber(pickedNumber)
@@ -17,6 +23,17 @@ export default function App() {
 
   function gameOverHandler() {
     setGameIsOver(true)
+  }
+
+  if (!fontsLoaded) {
+    return (
+      <LinearGradient
+        colors={[Colors.primary700, Colors.accent500]}
+        style={styles.rootScreen}
+      >
+        <Text>Loading App</Text>
+      </LinearGradient>
+    )
   }
 
   let screen = <StartGameScreen onPickedNumber={pickedNumberHandler} />
@@ -52,5 +69,10 @@ const styles = StyleSheet.create({
   },
   backgroundImage: {
     opacity: 0.15,
+  },
+  loadingStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 })
--- number-game/components/ui/PrimaryText.js
+++ number-game/components/ui/PrimaryText.js
@@ -9,6 +9,7 @@ export default PrimaryText
 
 const styles = StyleSheet.create({
   instructionText: {
+    fontFamily: 'open-sans',
     color: Colors.accent500,
     fontSize: 24,
   },
