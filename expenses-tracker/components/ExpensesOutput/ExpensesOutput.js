7715f142d8a24a85f1e449487e73247ce07b88be
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-06-03 10:02:59 -0300

    Starting styling of Expenses output

--- expenses-tracker/components/ExpensesOutput/ExpensesOutput.js
+++ expenses-tracker/components/ExpensesOutput/ExpensesOutput.js
@@ -1,15 +1,57 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 import ExpenesesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
+import { GlobalStyles } from "../../constants/styles";
 
-function ExpensesOutPut({ expenses, expensesPeriod }) {
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2021-12-19"),
+  },
+  {
+    id: "e2",
+    description: "A pair of pants",
+    amount: 89.99,
+    date: new Date("2021-12-20"),
+  },
+  {
+    id: "e3",
+    description: "Spider man 2 video game",
+    amount: 49.99,
+    date: new Date("2021-12-20"),
+  },
+  {
+    id: "e4",
+    description: "A shirt",
+    amount: 55.9,
+    date: new Date("2022-01-19"),
+  },
+  {
+    id: "e5",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2021-12-19"),
+  },
+];
+
+function ExpensesOutput({ expenses = DUMMY_EXPENSES, expensesPeriod }) {
   return (
-    <View>
+    <View style={styles.container}>
       <ExpenesesSummary expenses={expenses} periodName={expensesPeriod} />
-      <ExpensesList />
+      <ExpensesList expenses={expenses} />
     </View>
   );
 }
 
-export default ExpensesOutPut;
+export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+});
