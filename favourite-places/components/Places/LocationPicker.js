2b26336c24b82fca783033dfc361edac07316963
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-06-12 17:48:23 -0300

    Starting location feature

---
+++ favourite-places/components/Places/LocationPicker.js
@@ -0,0 +1,79 @@
+import { Alert, StyleSheet, View } from "react-native";
+import {
+  getCurrentPositionAsync,
+  useForegroundPermissions,
+  PermissionStatus,
+} from "expo-location";
+
+import OutlinedButton from "../UI/OutlinedButton";
+import { Colors } from "../../constants/colors";
+
+function LocationPicker() {
+  const [locationPermissionInformation, requestPermission] =
+    useForegroundPermissions();
+
+  async function verifyPermissions() {
+    if (
+      locationPermissionInformation.status === PermissionStatus.UNDETERMINED
+    ) {
+      const permissionresponse = await requestPermission();
+
+      return permissionresponse.granted;
+    }
+
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient permissions!",
+        "You need to grant location permission to use this feature."
+      );
+      return false;
+    }
+
+    return true;
+  }
+
+  async function getLocationHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    const location = await getCurrentPositionAsync();
+    console.log(location);
+  }
+  function pickOnMapHandler() {}
+
+  return (
+    <View>
+      <View style={styles.mapPreview}></View>
+      <View style={styles.actions}>
+        <OutlinedButton onPress={getLocationHandler} icon="location">
+          Locate User
+        </OutlinedButton>
+        <OutlinedButton onPress={pickOnMapHandler} icon="map">
+          Pick on Map
+        </OutlinedButton>
+      </View>
+    </View>
+  );
+}
+
+export default LocationPicker;
+
+const styles = StyleSheet.create({
+  mapPreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  actions: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
